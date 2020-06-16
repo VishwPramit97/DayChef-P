a36547270014d295758b0bcb26543a475bca50d7
unknown <sanusinghrajput95@gmail.com>
2020-06-16 12:04:53 +0530

    source-1

---
+++ frontend1/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { componentFactoryName } from '@angular/compiler';
+import { WebsiteComponent } from './website/website.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+
+
+const routes: Routes = [
+ { path:"",component:WebsiteComponent,children:[
+                                 {   
+                                    path:"Register ",component:RegisterComponent
+                                 },
+                                 {   
+                                  path:"Login ",component:LoginComponent
+                                 }
+ ]}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend1/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WebsiteComponent } from './website/website.component';
+import { HeaderComponent } from './header/header.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WebsiteComponent,
+    HeaderComponent,
+    RegisterComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

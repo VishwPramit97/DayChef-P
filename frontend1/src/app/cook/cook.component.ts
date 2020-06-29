45e6490715833d382c93b00a604f5afdcbeeea11
unknown <sanusinghrajput95@gmail.com>
2020-06-29 11:29:00 +0530

    backend

--- frontend1/src/app/cook/cook.component.ts
+++ frontend1/src/app/cook/cook.component.ts
@@ -15,13 +15,12 @@ export class CookComponent implements OnInit {
   arriveprop;
   tarriveprop
 
-  constructor(private router:Router, private ck:CookconfirmComponent) { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
    booking()
    {
-    this.router.navigate(['/cookconfirm']);
-    this.ck.bookin({people:this.peopleprop,demand:this.Demandprop,time:this.tarriveprop,term:this.timeprop,startdate:this.arriveprop,enddate:this.earriveprop})
+    this.router.navigate(['/dashboard/cookconfirm'],{queryParams:{people:this.peopleprop,demand:this.Demandprop,time:this.tarriveprop,term:this.timeprop,startdate:this.arriveprop,enddate:this.earriveprop}});
    }
 }
--- frontend1/src/app/cookconfirm/cookconfirm.component.ts
+++ frontend1/src/app/cookconfirm/cookconfirm.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DataService } from '../data.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cookconfirm',
@@ -7,29 +8,37 @@ import { DataService } from '../data.service';
   styleUrls: ['./cookconfirm.component.css']
 })
 export class CookconfirmComponent implements OnInit {
-  @Input() peopleprop;
-  @Input() Demandprop;
-  @Input() timeprop;
-  @Input() earriveprop;
-  @Input() arriveprop;
-  @Input() tarriveprop;
-  firstnameprop;
+  firstnameprop1;
   chefprop1;
   pinprop1;
   stateprop1;
   cityprop1;
   addressprop1;
   emailprop1;
- 
-
-
-  constructor(private dst:DataService) { }
+  bookdetail;
+  bookdetail1;
+  bookdetail2;
+  bookdetail3;
+  bookdetail4;
+  bookdetail5;
+  constructor(private dst:DataService,private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.queryParamMap.subscribe((d)=>{
+      this.bookdetail=d.get("people");
+      this.bookdetail1=d.get("demand");
+      this.bookdetail2=d.get("time");
+      this.bookdetail3=d.get("term");
+      this.bookdetail4=d.get("startdate");
+      this.bookdetail5=d.get("enddate");
+  })
+  
+
   }
-  bookin(dt)
+ 
+  bookin()
   {
-    this.dst.confirm({dt,firstname:this.firstnameprop,chefno:this.chefprop1,pinno:this. pinprop1,state:this.stateprop1,city:this.cityprop1,addres:this.addressprop1,email:this.emailprop1})
+    this.dst.confirm({bdetail:this.bookdetail,bdetail1:this.bookdetail1,bdetail2:this.bookdetail2,bdetail3:this.bookdetail3,bdetail4:this.bookdetail4,bdetail5:this.bookdetail5,firstname:this.firstnameprop1,chefno:this.chefprop1,pinno:this. pinprop1,state:this.stateprop1,city:this.cityprop1,addres:this.addressprop1,email:this.emailprop1})
     .subscribe((response)=>{
       if(response.status=="ok")
       {
